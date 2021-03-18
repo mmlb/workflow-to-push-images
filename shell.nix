a798b83908819b11363411357c396afb24661ed3
Manuel Mendez <mmendez534@gmail.com>
2021-03-18 21:00:56 +0000

    nixfmt shell.nix

--- shell.nix
+++ shell.nix
@@ -1,25 +1,18 @@
 let _pkgs = import <nixpkgs> { };
-in
-{ pkgs ?
-  import
-    (_pkgs.fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      #branch@date: nixpkgs-unstable@2021-01-25
-      rev = "ce7b327a52d1b82f82ae061754545b1c54b06c66";
-      sha256 = "1rc4if8nmy9lrig0ddihdwpzg2s8y36vf20hfywb8hph5hpsg4vj";
-    }) { }
-}:
+in { pkgs ? import (_pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  #branch@date: nixpkgs-unstable@2021-01-25
+  rev = "ce7b327a52d1b82f82ae061754545b1c54b06c66";
+  sha256 = "1rc4if8nmy9lrig0ddihdwpzg2s8y36vf20hfywb8hph5hpsg4vj";
+}) { } }:
 
 with pkgs;
-let
-  docker-ov = docker.override {
-    buildxSupport = true;
-  };
-in
+
 mkShell {
   buildInputs = [
-    docker-ov
+    (docker.override { buildxSupport = true; })
+    nixfmt
     nodePackages.prettier
   ];
 }
